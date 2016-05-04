89919b9f159f43f46ccc0a4ce043434dcf4275ba
Nissa Wollum <nissawollum@gmail.com>
2016-05-03 19:24:09 -0700

    Completes Multiple Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component} from 'angular2/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -11,20 +8,14 @@ export class Hero {
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" 
+      <li *ngFor="let hero of heroes"
       [class.selected]="hero === selectedHero"
       (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-    </div>`,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
     styles:[`
   .selected {
     background-color: #CFD8DC !important;
@@ -73,7 +64,8 @@ export class Hero {
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+directives: [HeroDetailComponent]
 
 })
 
